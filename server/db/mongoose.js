d0e1de6297d64fbd971f3f3e5aeb3b6c63007239
Myrzaliyev Turganbay <t.myrzaliyev@mail.ru>
2018-03-29 14:21:27 +0600

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 const mongoLocalURL = 'mongodb://localhost:27017/TodoApp';
-const mongoMLabUrl = 'mongodb://turganbay:turboturbo92@ds227469.mlab.com:27469/todo-app';
+const mongoMLabUrl = 'mongodb:/.../todo-app';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoMLabUrl);
